8c016acc381aa04b0649d558906000b64dc5282b
Eran Geva <eran.geva@gmail.com>
2026-03-04 13:43:18 -0100

    type signatures for CvElements and copy json to dist

--- lib/src/fiber/types.ts
+++ lib/src/fiber/types.ts
@@ -1,14 +1,40 @@
 import type { ReactNode } from "react";
+import type {} from "react/jsx-runtime";
+import type {} from "react/jsx-dev-runtime";
+import type signatures from "../data/opencv-signatures.json";
 
 export interface CvOpProps {
   children?: ReactNode;
   [param: string]: unknown;
 }
 
+type SigKeys = keyof typeof signatures;
+
+type CvElements = {
+  [K in SigKeys as `cv${K}`]: CvOpProps;
+} & {
+  [K in SigKeys as `cv${Capitalize<K>}`]: CvOpProps;
+} & {
+  [K in SigKeys as `cv_${K}`]: CvOpProps;
+} & {
+  cvImage: CvOpProps;
+  cv_image: CvOpProps;
+};
+
 declare module "react" {
   namespace JSX {
-    interface IntrinsicElements {
-      [key: string]: CvOpProps;
-    }
+    interface IntrinsicElements extends CvElements {}
+  }
+}
+
+declare module "react/jsx-runtime" {
+  namespace JSX {
+    interface IntrinsicElements extends CvElements {}
+  }
+}
+
+declare module "react/jsx-dev-runtime" {
+  namespace JSX {
+    interface IntrinsicElements extends CvElements {}
   }
 }
